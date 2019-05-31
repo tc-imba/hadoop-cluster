1f2dd63d15f47dd6cfa045c5c0d3808179fb3db9
tc-imba <liuyh615@sjtu.edu.cn>
2019-05-31 17:04:38 +0800

    fix: master/clean.sh missing compose file path

--- master/clean.sh
+++ master/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 echo "clean hadoop-master"
-docker-compose -p hadoop-master rm
+docker-compose -f "$SHELL_FOLDER/docker-compose.yml" -p hadoop-master rm
 docker network rm hadoop-net
